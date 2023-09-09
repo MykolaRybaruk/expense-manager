647567139d952f3d226877b12463d4f278ee27f5
Mykola Rybaruk <m.rybaruk@gmail.com>
2023-09-09 20:22:57 +0200

    added  dynamic chartbar

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+
+export default function Chart(props) {
+    return <div className="chart">
+        {props.dataPoints.map(dataPoint => <ChartBar
+                                                value={dataPoint.value}
+                                                maxValue={null}
+                                                label={dataPoint.label}
+                                                key={dataPoint.label}
+                                            />)}
+    </div>
+}
